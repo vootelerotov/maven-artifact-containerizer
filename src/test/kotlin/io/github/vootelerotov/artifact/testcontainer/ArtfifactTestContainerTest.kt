e03bd0d39cb81c656443845921a76182d8fd66f7
Vootele Rotov <vootele.rotov@gmail.com>
2022-03-09 08:28:41 +0200

    Re-structure test to allow for simple re-use of test artifacts
    We only need to build and install the artifact to Maven Local once.

--- src/test/kotlin/io/github/vootelerotov/artifact/testcontainer/ArtfifactTestContainerTest.kt
+++ src/test/kotlin/io/github/vootelerotov/artifact/testcontainer/ArtfifactTestContainerTest.kt
@@ -1,7 +1,11 @@
 package io.github.vootelerotov.artifact.testcontainer
 
 import org.jboss.shrinkwrap.resolver.api.maven.embedded.EmbeddedMaven
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.testcontainers.containers.wait.strategy.Wait
 import java.io.File
 import java.nio.file.Path
@@ -9,33 +13,50 @@ import java.time.Duration
 
 internal class ArtfifactTestContainerTest {
 
-  @Test
-  fun runnableJarFromArtifactInLocalMaven() {
-    publishToMavenLocal(Path.of("test-projects", "main-class-jar","pom.xml").toFile())
+  @Nested
+  @TestInstance(PER_CLASS)
+  inner class RunnableJar {
 
-    val container = ArtfifactTestContainer.fromArtifact(
-      "io.github.vootelerotov.test.projects:main-class-jar:1.0-SNAPSHOT"
-    ).build()
-      .withLogConsumer { println(it.utf8String) }
-      .waitingFor(Wait.forLogMessage(".*Started!.*", 1).withStartupTimeout(Duration.ofSeconds(1)))
-
-    container.use {
-      container.start() // Not asserting anything, relying on the #waitingFor to throw an error when Started! is not present
+    @BeforeAll
+    fun publishRunnableJarToMavenLocal() { // io.github.vootelerotov.test.projects:main-class-jar:1.0-SNAPSHOT
+      publishToMavenLocal(Path.of("test-projects", "main-class-jar", "pom.xml").toFile())
     }
+
+    @Test
+    fun runnableJarFromArtifactInLocalMaven() {
+      val container = ArtfifactTestContainer.fromArtifact(
+        "io.github.vootelerotov.test.projects:main-class-jar:1.0-SNAPSHOT"
+      ).build()
+        .withLogConsumer { println(it.utf8String) }
+        .waitingFor(Wait.forLogMessage(".*Started!.*", 1).withStartupTimeout(Duration.ofSeconds(1)))
+
+      container.use {
+        container.start() // Not asserting anything, relying on the #waitingFor to throw an error when Started! is not present
+      }
+    }
+
   }
 
-  @Test
-  fun jarWithDependencyFromArtifactInLocalMaven() {
-    publishToMavenLocal(Path.of("test-projects", "jar-with-dependencies","pom.xml").toFile())
+  @Nested
+  @TestInstance(PER_CLASS)
+  inner class JarWithDependencies {
 
-    val container = ArtfifactTestContainer.fromArtifact(
-      "io.github.vootelerotov.test.projects:jar-with-dependencies:1.0-SNAPSHOT"
-    ).withClassName("io.github.vootelerotov.jar.with.dependencies.Main").build()
-      .withLogConsumer { println(it.utf8String) }
-      .waitingFor(Wait.forLogMessage(".*Started!.*", 1).withStartupTimeout(Duration.ofSeconds(1)))
+    @BeforeAll
+    fun publishJarWithDependenciesToMavenLocal() { // io.github.vootelerotov.test.projects:jar-with-dependencies:1.0-SNAPSHOT
+      publishToMavenLocal(Path.of("test-projects", "jar-with-dependencies", "pom.xml").toFile())
+    }
 
-    container.use {
-      container.start() // Not asserting anything, relying on the #waitingFor to throw an error when Started! is not present
+    @Test
+    fun jarWithDependencyFromArtifactInLocalMaven() {
+      val container = ArtfifactTestContainer.fromArtifact(
+        "io.github.vootelerotov.test.projects:jar-with-dependencies:1.0-SNAPSHOT"
+      ).withClassName("io.github.vootelerotov.jar.with.dependencies.Main").build()
+        .withLogConsumer { println(it.utf8String) }
+        .waitingFor(Wait.forLogMessage(".*Started!.*", 1).withStartupTimeout(Duration.ofSeconds(1)))
+
+      container.use {
+        container.start() // Not asserting anything, relying on the #waitingFor to throw an error when Started! is not present
+      }
     }
   }
 
